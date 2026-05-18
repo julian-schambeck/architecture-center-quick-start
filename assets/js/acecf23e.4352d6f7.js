f376c50f4a37fae40e0b865e7de8778d96451b16
navyakhurana <navya.khurana@sap.com>
2026-05-18 14:20:58 +0530

    Deploy website - based on b6dbb92519e1bddc64a94e9fdd2a2447b684ac7e

--- assets/js/acecf23e.4352d6f7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[81903],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/news","blogTitle":"SAP Architecture Center News","authorsListPath":"/news/authors"}')}}]);
